21e22b85d4856566b50b58fec8b30d6430fd6850
Eduardo Lange <eduardoandrelange@gmail.com>
2024-09-22 18:35:23 -0300

    time to relax

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
 import Historico from './components/Historico/Historico'
 import Dashboard from './components/Dashboard/Dashboard'
+import { useState, useEffect } from 'react'
 
 const App = () => {
-	return (
+	const [historico, setHistorico] = useState([]) //para armazenar o estado do histórico
+  const [loading, setLoading] = useState(true) // Para indicar o estado de carregamento
+	const [error, setError] = useState(null) // Para armazenar erros, se houver
+
+	useEffect(() => {
+		// Simulando o fetch dos dados do arquivo JSON
+		fetch('/exemplos.json')
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error('Erro ao buscar o histórico: ' + response.statusText)
+				}
+				return response.json()
+			})
+			.then((data) => {
+				const historicoFiltrado = data.filter((item) => item.user === userOnline)
+				setHistorico(historicoFiltrado) //altera o estado do histórico para o array filtrado com os usuários online
+				setLoading(false) // Concluiu o carregamento
+			})
+			.catch((error) => {
+				setError(error.message) //retorna mensagem de erro
+				setLoading(false) // Concluiu o carregamento
+			})
+	}, [])
+
+	return (  //agora preciso fazer as parada de router, link e path
 		<>
-			{/* <Dashboard /> */}
-			<Historico />
+			<Dashboard />
+			<Historico historico={historico}/>
 		</>
 	)
 }
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,8 +1,20 @@
+import { useState, useEffect } from 'react'
+import Logos from "../Logos/Logos"
+import Historico from '../Historico/Historico'
 
 
-const Dashboard = () => {
+const Dashboard = () => {  //na realidade aqui não é o main
+
+    const [historico, setHistorico] = useState([]) //para armazenar o estado do histórico
+
+
+
   return (
-    <div>Dashboard</div>
+   <main>
+        <Logos />   
+        {/* precido ajustar o handleFocusLogos (está no Historico) */}
+
+   </main>
   )
 }
 
